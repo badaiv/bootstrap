1dbc05d201c97da4d9330f7d5055a8c55687dc6c
Volodymyr Badai <vbadai@wattev.com>
2023-09-01 17:16:57 +0200

    switch to starship

--- zsh/.zinit.sh
+++ zsh/.zinit.sh
@@ -12,6 +12,9 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
 
+#export SPACESHIP_CONFIG="$HOME/bootstrap/zsh/.spaceshiprc.zsh"
+export STARSHIP_CONFIG="$HOME/bootstrap/zsh/.starship.toml"
+
 # Must Load OMZ Git library
 zinit snippet OMZL::git.zsh
 zinit snippet OMZL::theme-and-appearance.zsh
@@ -20,13 +23,17 @@ zinit snippet OMZL::theme-and-appearance.zsh
 #zinit snippet OMZP::git
 #zinit cdclear -q # <- forget completions provided up to this moment
 
-zinit light spaceship-prompt/spaceship-prompt
+#zinit light spaceship-prompt/spaceship-prompt
+zinit ice as"command" from"gh-r" \
+          atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+          atpull"%atclone" src"init.zsh"
+zinit light starship/starship
 zinit light zsh-users/zsh-autosuggestions
 #zinit light zsh-users/zsh-syntax-highlighting
 #zinit light marlonrichert/zsh-autocomplete
 #zinit light marlonrichert/zcolors
 zinit light zdharma-continuum/fast-syntax-highlighting
-zinit load  zdharma-continuum/history-search-multi-word
+zinit light  zdharma-continuum/history-search-multi-word
 zinit light djui/alias-tips
 zinit light mattmc3/zsh-safe-rm
 zinit light bobsoppe/zsh-ssh-agent
@@ -34,17 +41,17 @@ zinit light bobsoppe/zsh-ssh-agent
 
 zinit snippet OMZP::extract
 zinit snippet OMZP::brew
-zinit snippet OMZP::gcloud
+#zinit snippet OMZP::gcloud
 zinit snippet OMZP::command-not-found
 
 # ice svn to load full dir if there are several files
 zinit ice svn
 zinit snippet OMZP::aliases
 
-zinit ice svn
-zinit snippet OMZP::per-directory-history
+#zinit ice svn
+#zinit snippet OMZP::per-directory-history
 
-#check how to make working
+#todo check how to make working
 #zi atinit'autoload -Uz genpass-apple'
 #zinit snippet OMZ::plugins/genpass/genpass-apple
 #zinit snippet OMZP::genpass
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -17,6 +17,8 @@ source ~/.zinit.sh
 # load the rest of the configs
 source $HOME/bootstrap/zsh/.exports
 source $HOME/bootstrap/zsh/.aliases
-# source $HOME/bootstrap/zsh/.k8s_aliases
+source $HOME/bootstrap/zsh/.k8s_aliases
 # source $HOME/bootstrap/zsh/.kubectl_prompt
 
+# Created by `pipx` on 2023-08-03 11:49:21
+export PATH="$PATH:/Users/vbadai/.local/bin"
